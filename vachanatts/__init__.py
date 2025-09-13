3e3de97559533d63c5928348e954f1910e7628c7
VYNCX <117465410+VYNCX@users.noreply.github.com>
2025-09-13 16:08:46 +0700

    Update __init__.py

--- vachanatts/__init__.py
+++ vachanatts/__init__.py
@@ -13,8 +13,8 @@ def load_voice(voice_name="TH_F_1"):
         model_filename = f"voice/{voice_name}.onnx"
         config_filename = f"voice/{voice_name}.onnx.json"
 
-        model_path = hf_hub_download(repo_id="VIZINZTOR/Vachana-TTS", filename=model_filename,cache_dir="./model")
-        config_path = hf_hub_download(repo_id="VIZINZTOR/Vachana-TTS", filename=config_filename,cache_dir="./model")
+        model_path = hf_hub_download(repo_id="VIZINZTOR/VachanaTTS", filename=model_filename,cache_dir="./model")
+        config_path = hf_hub_download(repo_id="VIZINZTOR/VachanaTTS", filename=config_filename,cache_dir="./model")
         voice = Voice.load(model_path, config_path)
 
     return voice
@@ -42,3 +42,4 @@ def generate_speech(
 
 
     return "Speech Generated"
+
